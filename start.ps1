6824a118cc265c87803b37df5f9d3327a7b4a89c
nibons <imac5640@gmail.com>
2016-08-19 18:09:54 -0400

    added software prerequisites

--- start.ps1
+++ start.ps1
@@ -1,6 +1,27 @@
 [cmdletbinding()]
 Param($virtualizationType = 'hyper-v')
 
+    #region verify the prerequisite software
+
+    if(!$(choco --version)){
+        (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1') | out-file $home\installChocolatey.ps1
+        powershell.exe -ExecutionPolicy Bypass -file $("$home\installChocolatey.ps1")
+    }#verify that Chocolatey is installed
+
+    $packageList = @(
+        @{name = 'docker';version='1.12.0'}
+        @{name = 'docker-machine';version='0.8.0'}
+        @{name = 'boot2docker';version='1.6.2'}
+    )
+    foreach($package in $packageList){
+        write-verbose ("Installing {0}" -f $package.name)
+        choco install $package.name --version=$($package.version) -y | out-null
+    }
+    #endregion
+
+
+
+    #region create + configure the VM
 set-location $PSScriptRoot
 $dockerMachineName = [guid]::NewGuid() | select-object -ExpandProperty Guid
 
@@ -20,12 +41,16 @@ $dockerMachineName = [guid]::NewGuid() | select-object -ExpandProperty Guid
     #enable connection in this session
     write-verbose "Connecting to machine"
     docker-machine env $dockerMachineName | Invoke-Expression 
-        
+    #endregion
+
+    #region invoke docker build + compose
     docker build -t nibons/curler:latest .
 
     #run the service against PROD-edbc
     docker-compose up
     docker-compose down
+    #endregion
+
 
     #remove the docker-machine
     docker-machine rm --force $dockerMachineName
